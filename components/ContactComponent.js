01946af41beef39d5b0af4baf3f4c14c9ce9da2e
Aziz Akbarov <azizbakbarov@gmail.com>
2021-10-13 23:59:04 +0500

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,38 +1,28 @@
-import React, {Component} from 'react';
-import { ScrollView, Text } from 'react-native';
-import { Card } from 'react-native-elements';
+import React, { Component } from "react";
+import { ScrollView, Text } from "react-native";
+import { Card } from "react-native-elements";
+import * as Animatable from 'react-native-animatable';
 
-class Contact extends Component{
-    static navigationOptions = {
-        title: 'Contact Us'
-    };
+class Contact extends Component {
+  static navigationOptions = {
+    title: "Contact Us",
+  };
 
-    render(){
-        return(
-            <ScrollView>
-                <Card
-                title='Contact Information'
-                wrapperStyle={{margin: 20}}
-                >
-                    <Text style={{marginBottom:10}}>
-                            1 Nucamp Way
-                            </Text>
-                    <Text style={{marginBottom:10}}>
-                                Seattle, WA 98001
-                        </Text>
-                        <Text style={{marginBottom:10}}>
-                            U.S.A.</Text>
-                    <Text style={{marginBottom:10}}>
-                    Phone: 1-206-555-1234
-                    </Text>
-                    <Text style={{marginBottom:10}}>
-                    Email: campsites@nucamp.co
-                    </Text>
-                </Card>
-            </ScrollView>
-        );
-    }
-
+  render() {
+    return (
+      <ScrollView>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
+            <Text style={{ marginBottom: 10 }}>1 Nucamp Way</Text>
+            <Text style={{ marginBottom: 10 }}>Seattle, WA 98001</Text>
+            <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+            <Text style={{ marginBottom: 10 }}>Phone: 1-206-555-1234</Text>
+            <Text style={{ marginBottom: 10 }}>Email: campsites@nucamp.co</Text>
+          </Card>
+        </Animatable.View>
+      </ScrollView>
+    );
+  }
 }
 
 export default Contact;
