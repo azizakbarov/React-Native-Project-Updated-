c8983cc6cde2d1225e851341b9cc75617a6a0479
Aziz Akbarov <azizbakbarov@gmail.com>
2021-10-20 12:51:01 +0500

    Network Info

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -9,6 +9,7 @@ import {
   StyleSheet,
   Alert,
   PanResponder,
+  Share,
 } from "react-native";
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from "react-redux";
@@ -56,15 +57,26 @@ function RenderCampsite(props) {
           ],
           { cancelable: false }
         );
-      }
-
-     else if (recognizeComment(gestureState)) {
-         props.onShowModal()
+      } else if (recognizeComment(gestureState)) {
+        props.onShowModal();
       }
 
       return true;
     },
   });
+
+  const shareCampsite = (title, message, url) => {
+    Share.share(
+      {
+        title: title,
+        message: `${title}: ${message} ${url}`,
+        url: url,
+      },
+      {
+        dialogTitle: "Share " + title,
+      }
+    );
+  };
 
   if (campsite) {
     return (
@@ -100,6 +112,20 @@ function RenderCampsite(props) {
               raised
               reverse
               onPress={() => props.onShowModal()}
+            />
+            <Icon
+              name={"share"}
+              type="font-awesome"
+              color="#5637DD"
+              raised
+              reverse
+              onPress={() =>
+                shareCampsite(
+                  campsite.name,
+                  campsite.description,
+                  baseUrl + campsite.image
+                )
+              }
             />
           </View>
         </Card>
