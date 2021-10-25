c7762ba562fba42df6f99e9c4d01e3b3736e258e
Aziz Akbarov <azizbakbarov@gmail.com>
2021-10-25 20:54:08 +0500

    Final Touch

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -4,6 +4,8 @@ import { Input, CheckBox, Button, Icon } from "react-native-elements";
 import * as SecureStore from "expo-secure-store";
 import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
+import * as ImageManipulator from "expo-image-manipulator";
+import * as MediaLibrary from "expo-media-library";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { baseUrl } from "../shared/baseUrl";
 
@@ -162,8 +164,40 @@ class RegisterTab extends Component {
       if (!capturedImage.cancelled) {
         console.log(capturedImage);
         this.setState({ imageUrl: capturedImage.uri });
+        MediaLibrary.saveToLibraryAsync(this.state.imageUrl);
       }
     }
+  };
+
+  getImageFromGallery = async () => {
+    const cameraPermission = await Permissions.askAsync(Permissions.CAMERA);
+    const cameraRollPermission = await Permissions.askAsync(
+      Permissions.CAMERA_ROLL
+    );
+
+    if (
+      cameraPermission.status === "granted" &&
+      cameraRollPermission.status === "granted"
+    ) {
+      const capturedImage = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [1, 1],
+      });
+      if (!capturedImage.cancelled) {
+        console.log(capturedImage);
+        this.processedImage(capturedImage.uri)
+      }
+    }
+  };
+
+  processedImage = async (imgUri) => {
+    const processedImage = await ImageManipulator.manipulateAsync(
+      imgUri,
+      [{ resize: { width: 400 } }],
+      { format: SaveFormat.PNG }
+    );
+    console.log(processedImage);
+    this.setState({ imageUrl: capturedImage.uri });
   };
 
   handleRegister() {
@@ -194,6 +228,7 @@ class RegisterTab extends Component {
               style={styles.image}
             />
             <Button title="Camera" onPress={this.getImageFromCamera} />
+            <Button title="Gallery" onPress={this.getImageFromCamera} />
           </View>
           <Input
             placeholder="Username"
